2cb1d6f8a0e97a739eb08428ccbdbacf62426602
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-05-20 01:51:53 +0000

    Testing Tue May 20 2025 01:51:53 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex01/student/Programming.java
+++ chapter1/ex01/student/Programming.java
@@ -3,5 +3,6 @@ public class Programming
    public static void main(String args[])
    {
       System.out.println("I'm learning how to program in java.");
-   }
+   System.out.println("That's awesome!");}
 }
+
