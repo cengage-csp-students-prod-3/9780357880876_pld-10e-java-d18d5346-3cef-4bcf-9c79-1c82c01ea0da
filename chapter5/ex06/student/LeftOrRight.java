c99b006a93f327feded962bf2d0a9f8a15c1b1ac
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-06-26 05:19:15 +0000

    Testing Thu Jun 26 2025 05:19:15 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/LeftOrRight.java
+++ chapter5/ex06/student/LeftOrRight.java
@@ -16,6 +16,15 @@ public class LeftOrRight
 		leftOrRight = JOptionPane.showInputDialog("Enter L if you are left-handed, R if you are right-handed or X to quit.");
 
 		// Write your loop here.
+		while (leftOrRight.compareTo("X") != 0)
+		{
+			      if(leftOrRight.compareTo("L") == 0)
+				          leftTotal++;
+				  else
+					      rightTotal++;
+					      leftOrRight = JOptionPane.showInputDialog("Enter L if you are left-handed, R if you are right-handed or X to quit.");
+
+      }
 
 		// Output number of left or right-handed students.	
 		System.out.println("Number of left-handed students: " + leftTotal);
