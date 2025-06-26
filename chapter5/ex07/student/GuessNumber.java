3bf602d7059ff93e955a3305775bc06d5b80ece2
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-06-26 06:15:43 +0000

    Testing Thu Jun 26 2025 06:15:43 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex07/student/GuessNumber.java
+++ chapter5/ex07/student/GuessNumber.java
@@ -18,34 +18,34 @@ public class GuessNumber
 		// Prime the loop.
 		keepGoing = JOptionPane.showInputDialog("Do you want to guess a number? Enter Y or N");
       // Validate input.	
-	    while(keepGoing.compareTo("Y") != 0 && keepGoing.compareTo("N") != 0)
-		        keepGoing = JOptionPane.showInputDialog("Invalid Response." + " Please type Y or N.");
-		// Enter loop if they want to play.
-		while(keepGoing.compareTo("Y") == 0)
-		{
-			// Get user's guess.
-			stringNumber = JOptionPane.showInputDialog("I'm thinking of a number. .\n Try to guess by entering a number between 1 and 10");
-			userNumber = Integer.parseInt(stringNumber); 
-			// Validate input.	
-			while(userNumber < 1 || userNumber > 10)
-			{
-				stringNumber = JOptionPane.showInputDialog("Number must be in the range of 1 to 10: Please try again: ");
-				userNumber = Integer.parseInt(stringNumber);
-			}	
-            // Test to see if the user guessed correctly.
-			if(userNumber == number)
-			{
-				keepGoing = "N"; 
-				System.out.println("You are a genius. That's correct!");
-			}
-			else
-			{
-				keepGoing = JOptionPane.showInputDialog("That's not correct. Do you want to guess again? Enter Y or N");
-				// Validate input.
-				while(keepGoing.compareTo("Y") != 0 && keepGoing.compareTo("N") != 0)
-				    keepGoing = JOptionPane.showInputDialog("Invalid Response." + " Please type Y or N.");				
-			}
+	  while(keepGoing.compareTo("Y") != 0 && keepGoing.compareTo("N") != 0)
+		  keepGoing = JOptionPane.showInputDialog("Invalid Response." + " Please type Y or N.");
+	  // Enter loop if they want to play.
+	  while(keepGoing.compareTo("Y") == 0)
+	  {
+		  // Get user's guess.
+		  stringNumber = JOptionPane.showInputDialog("I'm thinking of a number. .\n Try to guess by entering a number between 1 and 10");
+		  userNumber = Integer.parseInt(stringNumber); 
+		  // Validate input.	
+		  while(userNumber < 1 || userNumber > 10)
+		  {
+			  stringNumber = JOptionPane.showInputDialog("Number must be in the range of 1 to 10: Please try again: ");
+			  userNumber = Integer.parseInt(stringNumber);
+		  }	
+          // Test to see if the user guessed correctly.
+		  if(userNumber == number)
+	      {
+			  keepGoing = "N"; 
+			  System.out.println("You are a genius. That's correct!");
+		  }
+		  else
+		  {
+			  keepGoing = JOptionPane.showInputDialog("That's not correct. Do you want to guess again? Enter Y or N");
+		      // Validate input.
+			  while(keepGoing.compareTo("Y") != 0 && keepGoing.compareTo("N") != 0)
+				  keepGoing = JOptionPane.showInputDialog("Invalid Response." + " Please type Y or N.");				
+		  }
 	
-		} // End of while loop.
-	}	
+	   } // End of while loop.
+    }	
 } // End of GuessNumber class.
