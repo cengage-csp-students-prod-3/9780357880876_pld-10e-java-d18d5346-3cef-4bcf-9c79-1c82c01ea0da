98fdd1213a63bb21cdf86e48c18833a41aed42db
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-06-26 20:04:49 +0000

    Testing Thu Jun 26 2025 20:04:49 GMT+0000 (Coordinated Universal Time)

--- chapter6/ex01/student/BattingAverage.java
+++ chapter6/ex01/student/BattingAverage.java
@@ -4,8 +4,10 @@ public class BattingAverage
 	public static void main(String args[])
 	{
 		// Declare a named constant for array size here.
+		final int MAX_AVERAGES = 8;
 
 		// Declare array here.
+		double averages[] = new double [MAX_AVERAGES];
 		
 		// Use this variable to store the batting average input by user.
 		double battingAverage;
@@ -20,11 +22,13 @@ public class BattingAverage
 		double total, average; 
 
 		// Write a loop to get batting averages from user and assign to array.
-		
+		for(int loopIndex = 0; loopIndex < MAX_AVERAGES; loopIndex++)
+		{
 		   averageString = JOptionPane.showInputDialog("Enter a batting average: ");
 		   battingAverage = Double.parseDouble(averageString);
 		   // Assign value to array.
-		   
+		   averages[loopIndex] = battingAverage;
+		}
 		
 		// Assign the first element in the array to be the minimum and the maximum.
 		min = averages[0];
@@ -33,18 +37,29 @@ public class BattingAverage
 		total = averages[0]; 
 		
 		// Write a loop here to access array values starting with averages[1]
-		
-		   // Within the loop test for minimum and maximum batting averages.
-		   
-		   // Also accumulate a total of all batting averages.
-		   
-		
-		   
+        for(int loopIndex = 1; loopIndex < MAX_AVERAGES; loopIndex++)
+		{
+			// Within the loop test for minimum and maximum batting averages.
+			if(averages[loopIndex] < min)
+			    min = averages[loopIndex];
+			if(averages[loopIndex] > max)
+			    max = averages[loopIndex];
+			// Also accumulate a total of all batting averages.
+			total =+ averages[loopIndex];
+		}
 		// Calculate the average of the 8 averages.
+		average = total / MAX_AVERAGES;
 		
+        // Print the averages stored in the averages array.
+		for(int loopIndex = 0; loopIndex < MAX_AVERAGES; loopIndex++)
+        {
+			System.out.println("averages[" + loopIndex + "] is: " + averages[loopIndex]);
+		}
+		// Print the maximum batting average, minimum batting average, and average batting average.
+		System.out.println("Minimum batting averages is " + min);
+		System.out.println("Maximum batting averages is " + max);
+		System.out.println("Average batting averages is " + average); 
 
-		// Print the averages stored in the averages array. 
 		
-		// Print the maximum batting average, minimum batting average, and average batting average. 
 	}
 }
