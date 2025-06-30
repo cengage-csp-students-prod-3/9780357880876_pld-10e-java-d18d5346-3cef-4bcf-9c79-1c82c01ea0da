f69d470451f8eb0b9be8796d70c9dc4908ad0508
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-06-30 20:33:01 +0000

    Reset Exercise Mon Jun 30 2025 20:33:01 GMT+0000 (Coordinated Universal Time)

--- chapter7/ex01/student/Flowers.java
+++ chapter7/ex01/student/Flowers.java
@@ -10,23 +10,17 @@ public class Flowers
 	public static void main(String args[]) throws Exception
 	{
 		// Declare variables here
-		String flowerName = "";
-		String sunOrShade = "";
-		String fileName = "flowers.dat";
 		
 		// Open input file.
-		FileReader fr = new FileReader(fileName,"flowers.dat");
 		
 		// Create BufferedReader object.
-		BufferedReader br = new BufferedReader(fr);
 		
+	
 		// Write while loop that reads records from file.
-		while((flowerName = br.readLine()) != null)
-		{
-                sunOrShade = br.readLine();
-		// Print flower name and the words sun or shade.
-		            System.out.println(flowerName + " is grown in the " + sunOrShade);	
-		}
+		
+      // Print flower name and the words sun or shade.
+			
+
 		br.close(); 	
 	} 
 } 
