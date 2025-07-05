2f54f6f9403884f37f63dcbcf3758f34723faa6e
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-07-05 21:55:25 +0000

    Testing Sat Jul 05 2025 21:55:25 GMT+0000 (Coordinated Universal Time)

--- chapter8/ex02/student/HouseholdSize.java
+++ chapter8/ex02/student/HouseholdSize.java
@@ -34,24 +34,46 @@ public class HouseholdSize
 			// Place value in array.
 			householdSizes[x] = householdSize;
 			// Calculate total of household sizes here
-			
+			sum += householdSizes [x];
 			x++;    // Get ready for next input item.
 			householdSizeString = JOptionPane.showInputDialog("Enter household size or 999 to quit: ");
 			householdSize = Integer.parseInt(householdSizeString); 
 		}  // End of input loop.
 		
-		
+		limit = x;
 		// Find the mean
-		
+		mean = sum / limit;
 		// This is the work done in the sortArray() method
-		
-		
+		x = 0;
+		pairsToCompare = limit - 1;
+		switchOccurred = true;
+
+	while(switchOccurred == true)
+	{
+		    x = 0;
+			switchOccurred = false;
+            
+			while(x < pairsToCompare)
+			{
+                    if(householdSizes[x] > householdSizes[x+1])
+					{
+						    temp = householdSizes[x + 1];
+							householdSizes[x+1] = householdSizes[x];
+							householdSizes[x] = temp;
+							switchOccurred = true;
+
+					}
+					x++;
+			}
+			pairsToCompare--;
+	}
 		// This is the work done in the displayArray() method
-		// Print the mean 
-
-		// Calculate the median
-
+		// Print the mean
+        System.out.println("Mean household size in Marengo is : " + mean);
+		
+        // Calculate the median
+		median = (limit - 1) / 2;
       // Print the median
-
+      System.out.println("Median household size in Marengo is : " + householdSizes[median]);
 	} // End of main() method.
 } // End of HouseholdSize class.
