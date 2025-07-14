c449802215b03fc3fa3d86dfbd7ab0966c16ecfa
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-07-14 21:16:23 +0000

    Testing Mon Jul 14 2025 21:16:23 GMT+0000 (Coordinated Universal Time)

--- chapter9/ex06/student/Cornwall.java
+++ chapter9/ex06/student/Cornwall.java
@@ -18,12 +18,34 @@ public class Cornwall
 		question = JOptionPane.showInputDialog(" Do you want a meal plan? Y or N: "); 
 		// Figure out which arguments to pass to the computeRate() method and 
 		// then call the computeRate() method
-		
+		if(question.equals("N"))
+		        rate = computerRate(days);
+		else if(question.equals("Y"))
+		{
+			    mealPlan = JOptionPane.showInputDialog("Enter meal plan A or C: ");
+				if(mealPlan.equals("A") || mealPlan.equals("C"))
+						rate = computeRate(days, mealPlan);
+		}
+
 		System.out.println("The rate for your stay is: " + rate);
 
 	} // End of main() method.
 	
-	
 	// Write computeRate methods here.
-	
+	public static double computerRate(int days)
+	{
+		    double total;
+			total = days * 99.99;
+			return total;
+	}
+
+	public static double computeRate(int days, String mealPlan)
+	{
+		    double total;
+			if(mealPlan.equals("A"))
+			        total = days * 169.00;
+			else    total = days * 112.00;
+			return total;
+	}
+
 } // End of Cornwall class.
