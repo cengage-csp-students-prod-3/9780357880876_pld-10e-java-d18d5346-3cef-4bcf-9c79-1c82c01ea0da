9baee5103c30d65f3281b1f1e94970b5981394d9
RiverGarland1 <garland.river@titans.easternflorida.edu>
2025-07-22 03:06:37 +0000

    Testing Tue Jul 22 2025 03:06:37 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex02/student/JavaQuiz.java
+++ chapter10/ex02/student/JavaQuiz.java
@@ -9,33 +9,33 @@ class JavaQuiz
 	JavaQuiz()
 	{
 		// Create a JLabel here named labelQuestion with appropriate text
-	
+	    JLabel labelQuestion = new JLabel("Do you like GUI programming? Vote Yes or No.");
 		// Create a JLabel here named labelYes with appropriate text
-		
+		JLabel labelYes = new JLabel("Click here to vote Yes");
 		// Create a JLabel here named labelNo with appropriate text
-		
+		JLabel labelNo = new JLabel("Click here to vote No");
 		// Create a final JButton here named buttonYes with appropriate text
-		
+		final JButton buttonYes = new JButton("Yes");
 		buttonYes.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				// Set the JPanel background color to yellow
-
+                myPanel.setBackground(Color.yellow);
 				
 			}
 		});
 
 		// Create a final JButton here named buttonNo with appropriate text
-
+        final JButton buttonNo = new JButton("No");
 		buttonNo.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				// Set the JPanel background color to red
-
+                myPanel.setBackground(Color.red);
 			}
 		});
 		
 		// Create a JPanel named myPanel
+		myPanel = new JPanel();
 		
-
 		myPanel.add(labelQuestion);		
 
 		myPanel.add(labelYes);
